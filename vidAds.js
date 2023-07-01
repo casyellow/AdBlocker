e74a40f5af523eaf5992a79c44914194cb34a7af
Gab Castelo <60293733+casyellow@users.noreply.github.com>
2023-07-01 13:15:00 -0400

    First code commit!
    Gotta fix Uncaught TypeError.

---
+++ vidAds.js
@@ -0,0 +1,22 @@
+if (window.location.href.includes("youtube.com")) {
+	console.log("Checking for ads...");
+	time = Math.trunc(document.getElementsByTagName('video')[0].currentTime);
+	var func = setInterval(function () {
+		if (document.getElementsByClassName("video-ads")[0].innerHTML !== "") {
+			var banner = false;
+			for (var i = 0; i < document.getElementsByClassName("ytp-ad-overlay-close-button").length; i++) {
+				document.getElementsByClassName("ytp-ad-overlay-close-button")[i].click();
+				banner = true;
+			}
+			if (banner === true)
+				console.log("Skippable ad detected in video at " + time + " seconds.");
+			if (banner === false) {
+				console.log("Unskippable ad detected in video at " + time + " seconds.");
+				document.getElementsByClassName("html5-main-video")[0].currentTime = document.getElementsByClassName("html5-main-video")[0].duration;
+				document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+			}
+		}
+		else // Uncaught TypeError: Cannot read properties of undefined (reading 'currentTime')
+			time = Math.trunc(document.getElementsByTagName('html-main-video')[0].currentTime);
+	}, 500);
+}
