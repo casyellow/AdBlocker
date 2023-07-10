d6e75c20fb4813a00b84e8a332262e8fdc9bda7b
Gab Castelo <60293733+casyellow@users.noreply.github.com>
2023-07-10 10:23:04 -0400

    Update vidAds.js commenting all uses of time var to avoid error

--- vidAds.js
+++ vidAds.js
@@ -1,6 +1,6 @@
 if (window.location.href.includes("youtube.com")) {
 	console.log("Checking for ads...");
-	time = Math.trunc(document.getElementsByTagName('video')[0].currentTime);
+	//time = Math.trunc(document.getElementsByTagName('video')[0].currentTime);
 	var func = setInterval(function () {
 		if (document.getElementsByClassName("video-ads")[0].innerHTML !== "") {
 			var banner = false;
@@ -8,15 +8,15 @@ if (window.location.href.includes("youtube.com")) {
 				document.getElementsByClassName("ytp-ad-overlay-close-button")[i].click();
 				banner = true;
 			}
-			if (banner === true)
-				console.log("Skippable ad detected in video at " + time + " seconds.");
+			//if (banner === true)
+				//console.log("Skippable ad detected in video at " + time + " seconds.");
 			if (banner === false) {
-				console.log("Unskippable ad detected in video at " + time + " seconds.");
+				//console.log("Unskippable ad detected in video at " + time + " seconds.");
 				document.getElementsByClassName("html5-main-video")[0].currentTime = document.getElementsByClassName("html5-main-video")[0].duration;
 				document.getElementsByClassName("ytp-ad-skip-button")[0].click();
 			}
 		}
-		else // Uncaught TypeError: Cannot read properties of undefined (reading 'currentTime')
-			time = Math.trunc(document.getElementsByTagName('html-main-video')[0].currentTime);
+		//else // Uncaught TypeError: Cannot read properties of undefined (reading 'currentTime')
+			//time = Math.trunc(document.getElementsByTagName('html-main-video')[0].currentTime);
 	}, 500);
 }
